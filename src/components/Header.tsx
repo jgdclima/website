209a37fffaa2d42bf1b0475bb16368edc5ee247b
João Lima <theotherlima@gmail.com>
2020-05-19 14:15:01 -0700

    removed mui-button from header items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,39 @@
 import React, { ReactElement } from 'react'
-import {Grid, Button} from '@material-ui/core'
-import {makeStyles, createStyles, Theme} from '@material-ui/core/styles'
+import { Grid, Button } from '@material-ui/core'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import { spacing } from '@material-ui/system'
 
 
-type HeaderProps ={
+type HeaderProps = {
     children: React.ReactChild[];
 }
 
 // const theme = useTheme()
-const useStyles = makeStyles((theme: Theme) => createStyles ({
+const useStyles = makeStyles((theme: Theme) => createStyles({
     root: {
         backgroundColor: theme.palette.primary.dark,
-        // paddingLeft: theme.spacing(1),
-        // paddingRight: theme.spacing(1),
-        // paddingTop: theme.spacing(2),
-        // paddingBottom: theme.spacing(2)
         height: '10vh'
     },
+    link: {
+        textDecoration: "none",
+        color: '#fff'
+    }
 }))
 
 
-export default function Header({children}: HeaderProps): ReactElement {
+export default function Header({ children }: HeaderProps): ReactElement {
     const classes = useStyles()
 
     return (
-            <Grid container xs={12} className={classes.root}>
-                <Grid item xs={3}><Button> {children[0]}</Button> </Grid>
-                <Grid item xs={6}></Grid>
-                {children.map(child => {
-                    if (children.indexOf(child) > 0)
-                        return <Grid item xs><Button> {child}</Button></Grid>
-                    return ''
-                })}
-            </Grid>
+        <Grid container xs={12} className={classes.root} alignItems="center">
+            <Grid item xs={3}>{children[0]} </Grid>
+            <Grid item xs={6}></Grid>
+            {children.map(child => {
+                if (children.indexOf(child) > 0)
+                    return <Grid item xs>{child}</Grid>
+                return ''
+            })}
+        </Grid>
     )
 }
 
