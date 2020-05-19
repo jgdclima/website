874f7a48f31a92bc4b5e2e492089b3ddf0fd87ec
João Lima <theotherlima@gmail.com>
2020-05-19 11:07:00 -0700

    Created Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import React, { ReactElement } from 'react'
+import {Grid, Button} from '@material-ui/core'
+import {makeStyles, createStyles, Theme} from '@material-ui/core/styles'
+import { spacing } from '@material-ui/system'
+
+
+type HeaderProps ={
+    children: React.ReactChild[];
+}
+
+// const theme = useTheme()
+const useStyles = makeStyles((theme: Theme) => createStyles ({
+    root: {
+        backgroundColor: theme.palette.primary.dark,
+        // paddingLeft: theme.spacing(1),
+        // paddingRight: theme.spacing(1),
+        // paddingTop: theme.spacing(2),
+        // paddingBottom: theme.spacing(2)
+        height: '10vh'
+    },
+}))
+
+
+export default function Header({children}: HeaderProps): ReactElement {
+    const classes = useStyles()
+
+    return (
+            <Grid container xs={12} className={classes.root}>
+                <Grid item xs={3}><Button> {children[0]}</Button> </Grid>
+                <Grid item xs={6}></Grid>
+                {children.map(child => {
+                    if (children.indexOf(child) > 0)
+                        return <Grid item xs><Button> {child}</Button></Grid>
+                    return ''
+                })}
+            </Grid>
+    )
+}
+
+// export default withStyles(styles)(Header)
